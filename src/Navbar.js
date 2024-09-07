bb4f565c6ab4263e5e19a41a95c0fad08be09a09
markall201 <markall201@hotmail.co.uk>
2024-09-07 15:21:25 +0100

    Fixed navbar links for routing

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,5 +1,10 @@
 // the Navbar component
 
+// to do React routing, we need to import and use the Link component instead of standard hyperlinks.
+// So instead of <a href="link">text</a> we have <Link to="link">text</Link>
+// The Link object does compile these as anchor tags but they're specially controlled by any parent Router in the DOM.
+import { Link } from 'react-router-dom';
+
 function Navbar() {
   const title = "";
 
@@ -8,8 +13,8 @@ function Navbar() {
     <nav className='navbar'>
         <h1>{ title }</h1>
         <div className="links">
-            <a href="/">Home</a>
-            <a href="/about">About</a>
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
         </div>
     </nav>
   );
