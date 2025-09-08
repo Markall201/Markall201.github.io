bbd5f2ea56670070a19719c9656a8052ee6d30c9
markall201 <markall201@hotmail.co.uk>
2025-09-08 21:55:02 +0100

    <Navigate> element to redirect no match route here's hoping this works

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-          <Route path="*" element={<Home />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
         </div>
       </div>
